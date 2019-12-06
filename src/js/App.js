015852fc10ce043df0572f27642090e92721cb8b
Arjen Kroeze <git@arjenkroeze.nl>
2019-12-07 00:09:48 +0100

    WIP

--- src/js/App.js
+++ src/js/App.js
@@ -9,6 +9,7 @@ function App() {
 
     const [registrations, setRegistrations] = useState([])
 
+    // EFfect to fetch all registrations from the database
     useEffect(() => {
         const fetchData = async () => {
             const registrationsSnapshot = await database.collection('registrations').get()
@@ -24,57 +25,63 @@ function App() {
 
     const [modal, setModal] = useState(false)
     const toggleModal = () => setModal(!modal)
-    console.log(modal)
 
-    // Work with full weeks, so start the week always on a monday
-    // Therefore we copy the startDate and change it to the monday before
+    // Work with full weeks, so start the week always on a monday.
+    // Create a new date so this can be changed to the monday before the startDate.
     const startDateCopy = new Date(startDate)
     const startDateCopyDay = startDateCopy.getDay()
     const monday = 1
 
+    // Calculate the difference between the startDate's day and the monday before this date.
+    // Then change the startDate to that day.
     if (startDateCopyDay !== monday) {
-        const offset = startDateCopyDay === 0 ? 6 : startDateCopyDay - 1
-        startDateCopy.setTime(startDateCopy.getTime() - 1000 * 60 * 60 * 24 * offset)
+        const offset = [6, 0, 1, 2, 3, 4, 5]
+        startDateCopy.setDate(startDateCopy.getDate() - offset[startDateCopyDay])
     }
 
-    // Also copy the endDate and change it to the sunday after
+    // Create a new date so this can be changed to the sunday after the endDate.
     const endDateCopy = new Date(endDate)
     const endDateCopyDay = endDateCopy.getDay()
     const sunday = 0
 
+    // Calculate the difference between the endDate's day and the sunday after this date.
+    // Then change the endDate to that sunday.
     if (endDateCopyDay !== sunday) {
-        const offset = {
-            '1': 6,
-            '2': 5,
-            '3': 4,
-            '4': 3,
-            '5': 2,
-            '6': 1,
-        }
-
-        endDateCopy.setTime(endDateCopy.getTime() + 1000 * 60 * 60 * 24 * offset[endDateCopyDay])
+        const offset = [0, 6, 5, 4, 3, 2, 1]
+        endDateCopy.setDate(endDateCopy.getDate() + offset[endDateCopyDay])
     }
 
-    // Calculate the difference in days
+    // Calculate the difference in days between the startDate and endDate
     const numberOfDays = (endDateCopy.getTime() - startDateCopy.getTime()) / 1000 / 60 / 60 / 24
 
-    // Sum up the weeks...
+    // Set an array to store the weeks
     const weeks = []
 
+    // For every day, do the following...
     for (let i = 0; i < numberOfDays; i++) {
-        // Create a week for every 7 days...
+        // Create a week for every 7 days
         if (i % 7 === 0) {
+            // Create the start date of the week which is the startDate plus the difference in days
             const weekStartDate = new Date(startDateCopy)
-            weekStartDate.setTime(weekStartDate.getTime() + i * 1000 * 60 * 60 * 24)
+            weekStartDate.setDate(weekStartDate.getDate() + i)
 
+            // Create the end date of the week, which is the start date + 6 days
             const weekEndDate = new Date(weekStartDate)
-            weekEndDate.setTime(weekEndDate.getTime() + 1000 * 60 * 60 * 24 * 6)
+            weekEndDate.setDate(weekEndDate.getDate() + 6)
 
             weeks.push(<Week key={i} startDate={weekStartDate} endDate={weekEndDate} />)
         }
     }
 
-    return <AppContext.Provider value={{ startDate, endDate, registrations, setModal }}>{weeks}</AppContext.Provider>
+    if (!registrations.length) {
+        return null
+    }
+
+    return (
+        <AppContext.Provider value={{ startDate, endDate, registrations, setModal }}>
+            {weeks}
+        </AppContext.Provider>
+    )
 }
 
 export default App
--- src/js/Week.js
+++ src/js/Week.js
@@ -7,33 +7,52 @@ function Week({ startDate, endDate }) {
     const { registrations } = useContext(AppContext)
     const [hover, setHover] = useState(-1)
 
-    let dayHeaders = []
+    // Array to store the title of the days
+    const dayTitles = [<th key="0"></th>]
 
+    // For every day of the week...
     for (let x = 0; x < 7; x++) {
+        // Create the day's date
         const dayDate = new Date(startDate)
         dayDate.setDate(dayDate.getDate() + x)
 
-        dayHeaders.push(
+        // Use the Intl API to format
+        const formattedDayDate = new Intl.DateTimeFormat('nl-NL', {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'short',
+        }).format(dayDate)
+
+        // Add to the array
+        dayTitles.push(
             <th key={dayDate.getTime()} className="week-cell-header">
-                {new Intl.DateTimeFormat('nl-NL', {
-                    weekday: 'long',
-                    day: 'numeric',
-                    month: 'short',
-                }).format(dayDate)}
+                {formattedDayDate}
             </th>
         )
     }
 
+    const formattedStartDate = new Intl.DateTimeFormat('nl-NL', {
+        day: 'numeric',
+        month: 'long',
+    }).format(startDate)
+
+    const formattedEndDate = new Intl.DateTimeFormat('nl-NL', {
+        day: 'numeric',
+        month: 'long',
+    }).format(endDate)
+
+    // Array to store the hours
     let hours = []
 
     // For every hour in a day...
     for (let i = 0; i < 24; i++) {
-        // Set the date's hour
-        const date = new Date(startDate).setHours(i)
+        // Create a new date that will increase by the hour
+        const hourDate = new Date(startDate).setHours(i)
 
+        // Array to store the days
         let days = []
 
-        // Render table cells for the first column
+        // Every day starts with a cell that contains the hour notation
         days.push(
             <td key={i} className={`week-hour${hover === i ? ` bg-light` : ``}`}>
                 {i}.00 - {i + 1}.00
@@ -42,18 +61,28 @@ function Week({ startDate, endDate }) {
 
         // For every day in a week...
         for (let j = 0; j < 7; j++) {
-            // Set the day
-            const dayDate = new Date(date)
+            // Create the day date which is derived from the hour date
+            const dayDate = new Date(hourDate)
             dayDate.setDate(dayDate.getDate() + j)
 
-            // Find the registration
+            // Find registrations on this date
             const filteredRegistrations = registrations.filter(registration => {
+                // Time is stored in the Firestore by seconds.
+                // Multiply by 1000 to create a date in milliseconds.
                 const registrationDate = new Date(registration.date.seconds * 1000)
-                return registrationDate.getTime() === dayDate.getTime() ? registrationDate : null
+                // Return the date if a match is found, else filter
+                return registrationDate.getTime() === dayDate.getTime() ? registrationDate : false
             })
 
             // Render a column for every day
-            days.push(<Day key={dayDate.getTime()} date={dayDate} hour={i} registrations={filteredRegistrations} />)
+            days.push(
+                <Day
+                    key={dayDate.getTime()}
+                    date={dayDate}
+                    hour={i}
+                    registrations={filteredRegistrations}
+                />
+            )
         }
 
         // Render a row for every hour
@@ -62,23 +91,10 @@ function Week({ startDate, endDate }) {
 
     return (
         <WeekContext.Provider value={{ setHover }}>
-            <h2 className="text-center">
-                {new Intl.DateTimeFormat('nl-NL', {
-                    day: 'numeric',
-                    month: 'long',
-                }).format(startDate)}{' '}
-                -{' '}
-                {new Intl.DateTimeFormat('nl-NL', {
-                    day: 'numeric',
-                    month: 'long',
-                }).format(endDate)}
-            </h2>
+            <h2 className="text-center">{`${formattedStartDate} - ${formattedEndDate}`}</h2>
             <table className="week">
                 <thead className="week-header">
-                    <tr>
-                        <th></th>
-                        {dayHeaders}
-                    </tr>
+                    <tr>{dayTitles}</tr>
                 </thead>
                 <tbody className="week-body">{hours}</tbody>
             </table>
